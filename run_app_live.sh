6c23eebb6402c8be6bae389b1ed94972803f75d3
flossverse <github@xrsystems.uk>
2024-09-11 16:14:06 +0100

    test new gitignore

--- run_app_live.sh
+++ run_app_live.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the Perplexity API key
-export PERPLEXITY_API_KEY="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"
+export PERPLEXITY_API_KEY="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"
 
 # Check if all arguments are provided
 if [ "$#" -ne 4 ]; then
